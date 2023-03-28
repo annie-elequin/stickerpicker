2c3fff11f78a20a4aa2581bf2677bfc7a4ebadda
Annie Elequin <vaelequin@gmail.com>
2023-03-28 11:33:04 +0200

    update

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '99ced887'
-version = '0.1.0+dev.99ced887'
-linkified_version = '0.1.0+dev.[99ced887](https://github.com/maunium/stickerpicker/commit/99ced8878aaf89a47dd8bdb592025ae2160d6f07)'
+git_revision = '651c58d7'
+version = '0.1.0+dev.651c58d7'
+linkified_version = '0.1.0+dev.[651c58d7](https://github.com/maunium/stickerpicker/commit/651c58d746f841c35ffdaa264ea61cc9498b39b0)'
